4f00f7a331c09d0d0e51c3d9cd7533d030317bb0
Daniel Lemos <daniel.lemos.jose1998@gmail.com>
2024-02-11 00:13:32 +0100

    componant2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,24 @@
 function PokemonCard() {
+    const pokemon = pokemonList[1]
     return <figure className="carte-pokemon">
-                <img className="image-pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-                <figcaption className='nom-pokemon'>Bulbasaur</figcaption>
+                {pokemon.imgSrc === undefined ? <p>No image</p> : <img className="image-pokemon" src={pokemon.imgSrc} alt={pokemon.name} />}
+                <figcaption className='nom-pokemon'>{pokemon.name}</figcaption>
             </figure>
 }
+
+const pokemonList = [
+    {
+        name: "balbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+    },
+    {
+        name: "mew",
+        
+    },
+    {
+        name: "Salameche",
+        imgSrc: "https://www.pokepedia.fr/images/thumb/8/89/Salam%C3%A8che-RFVF.png/800px-Salam%C3%A8che-RFVF.png"
+    }
+]
 
 export default PokemonCard;
