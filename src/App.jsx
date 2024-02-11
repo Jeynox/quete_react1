c13dca435897b623cb5e22d3bd6d5f7b22f929b6
Daniel Lemos <daniel.lemos.jose1998@gmail.com>
2024-02-11 01:02:38 +0100

    props.1

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,27 @@ import MyTitle from "./components/MyTitle";
 import PokemonCard from "./components/PokemonCard";
 import './App.css';
 
+const pokemonList = [
+  {
+      name: "balbasaur",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+  },
+  {
+      name: "mew",
+      
+  },
+  {
+      name: "Salameche",
+      imgSrc: "https://www.pokepedia.fr/images/thumb/8/89/Salam%C3%A8che-RFVF.png/800px-Salam%C3%A8che-RFVF.png"
+  }
+]
+
 function App() {
+  const pokemon = pokemonList[2];
   return (
     <div>
       <MyTitle/>
-      <PokemonCard/>
+      <PokemonCard pokemon={pokemon} />
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,24 +1,10 @@
-function PokemonCard() {
-    const pokemon = pokemonList[1]
+function PokemonCard(props) {
+    console.log(props)
+
     return <figure className="carte-pokemon">
-                {pokemon.imgSrc === undefined ? <p>No image</p> : <img className="image-pokemon" src={pokemon.imgSrc} alt={pokemon.name} />}
-                <figcaption className='nom-pokemon'>{pokemon.name}</figcaption>
+                {props.pokemon.imgSrc === undefined ? <p>No image</p> : <img className="image-pokemon" src={props.pokemon.imgSrc} alt={props.pokemon.name} />}
+                <figcaption className='nom-pokemon'>{props.pokemon.name}</figcaption>
             </figure>
 }
-
-const pokemonList = [
-    {
-        name: "balbasaur",
-        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-    },
-    {
-        name: "mew",
-        
-    },
-    {
-        name: "Salameche",
-        imgSrc: "https://www.pokepedia.fr/images/thumb/8/89/Salam%C3%A8che-RFVF.png/800px-Salam%C3%A8che-RFVF.png"
-    }
-]
 
 export default PokemonCard;
